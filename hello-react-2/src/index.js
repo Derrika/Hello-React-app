74e76830681209d77f97e839b2a0b5df21c55206
Derrika <coley.derrika79@gmail.com>
2022-05-12 17:10:12 -0500

    updated index.js using props

--- hello-react-2/src/index.js
+++ hello-react-2/src/index.js
@@ -3,26 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 
-
-
-function Hello({library, message, number}) {
-  //console.log(props);
+function Lake(props) {
   return (
-    <div>
-      <h1>Welcome to {library}!</h1>
-      <p>{message}</p>
-      <p>{number} Props total</p>
-    </div>
+      <h1>{props.name}</h1>
   )
 }
 
+function App() {
+  return (
+    <div>
+      <Lake name="Lake Tahoe"/>
+      <Lake name="Angora Lake"/>
+      <Lake name="Shirley Lake"/>
+    </div>
+  );
+}
 
 ReactDOM.render(
- <Hello 
- library="React" 
- message="Add dynamic data!"
- number={3}
- />,
+ <App/>,
   document.getElementById('root')
 );
 
