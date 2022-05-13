63909062622490abc1d2aa73f025ce065064c282
Derrika <coley.derrika79@gmail.com>
2022-05-13 12:41:56 -0500

    Update: useState

--- hello-react-2/src/index.js
+++ hello-react-2/src/index.js
@@ -1,28 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
 
-function Lake(){
+
+function App({}){
+  const [year, setYear] = useState(2050);
+  const [manager, setManager] = useState("Alex");
+  const [status, setStatus] = useState("Open");
+
   return (
-    <h1>Lake</h1>
+    <><div>
+        <h1>{year}</h1>
+        <button onClick={() => setYear(year + 1)}>New Year!</button>
+      </div>
+      <div>
+        <h1>Manager on Duty: {manager}</h1>
+        <button onClick={() => setManager("Rachel")}>
+          New Manager
+        </button>
+      </div>
+      <div>
+        <h1>Status: {status}</h1>
+        <button onClick={() => setStatus("Open")}>
+          Open
+        </button>
+        <button onClick={() => setStatus("Back is 5")}>
+          Break
+        </button>
+        <button onClick={() => setStatus("Closed")}>
+          Closed
+        </button>
+      </div></>
   )
 }
-
-function SkiResort() {
-  return (
-    <h1>Ski Resort</h1>
-  )
-}
-
 
 
 ReactDOM.render(
- <>
-  <Lake/>
-  <SkiResort/>
-  </>,
+ <App />,
   document.getElementById('root')
 );
+
+
+
 
 
