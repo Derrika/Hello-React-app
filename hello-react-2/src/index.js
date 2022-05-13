32c693d37b04b9c323361af1478c71251b4eb0f5
Derrika <coley.derrika79@gmail.com>
2022-05-12 19:35:36 -0500

    made changes using React.

--- hello-react-2/src/index.js
+++ hello-react-2/src/index.js
@@ -3,44 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
 
-function Lake({name}){
+function Lake(){
   return (
-    <div>
-      <h1>Visit {name}!</h1>
-    </div>
+    <h1>Lake</h1>
   )
 }
 
-function SkiResort({name}) {
+function SkiResort() {
   return (
-    <div>
-      <h1>Visit {name}!</h1>
-    </div>
+    <h1>Ski Resort</h1>
   )
 }
 
 
-function App(props) {
-  return (
-    <div>
-      {(props.season === "summer" ? (
-        <Lake name="Jenny Lake" /> 
-        ) : props.season === "winter" ? (
-          <SkiResort name="Jackson Hole Mountain Resort" />
-        ) : (
-          <h1>
-            Come back in the winter or summer!
-          </h1>
-        )
-      
-      )}
-    </div>
-  )
- 
-}
 
 ReactDOM.render(
- <App season="fall"/>,
+ <>
+  <Lake/>
+  <SkiResort/>
+  </>,
   document.getElementById('root')
 );
 
