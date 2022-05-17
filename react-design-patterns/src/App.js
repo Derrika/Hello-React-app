e07dbeb6c54858e4a35d1c66b563494e8ff33e5c
Derrika <coley.derrika79@gmail.com>
2022-05-17 15:44:28 -0500

    update: Reusable list

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -1,15 +1,89 @@
 import React from 'react';
 
-import { SplitScreen } from './SplitScreen';
+import { RegularList } from './RegularList';
+import { SmallPersonListItem } from "./people/SmallPersonListItem";
+import { LargePersonListItem } from './people/LargePersonListItem';
 
-const LeftHandCompenent = ({ name }) => {
+//import { SplitScreen } from './SplitScreen';
+
+const people = [
+  {
+    name: 'John Doe',
+    age: 54,
+    hairColor: 'brown',
+    hobbies: ['swimming', 'bicycling', 'video games'],
+  },
+
+  {
+    name: 'Brenda Smith',
+    age: 33,
+    hairColor: 'black',
+    hobbies: ['golf', 'mathematics'],
+  },
+
+  {
+    name: 'Jane Garcia',
+    age: 27,
+    hairColor: 'blonde',
+    hobbies: ['biology', 'medicine', 'gymnastics'],
+  }
+];
+
+const products = [
+  {
+    name: 'Flat-Screen Tv',
+    price: '$300',
+    description: 'Huge LCD screen, a great deal',
+    rating: 4.5,
+  },
+
+  {
+    name: 'Basketball',
+    price: '$10',
+    description: 'Just like the pros use',
+    rating: 3.8,
+  },
+
+  {
+    name: 'Running Shoes',
+    price: '$120',
+    description: 'State-of-the-art technology for optimum',
+    rating: 4.2,
+  }
+];
+
+function App() {
+    return (
+       <>
+       <RegularList 
+          items={people}
+          resourceName="person"
+          itemComponent={SmallPersonListItem}
+       />
+      <RegularList 
+        items={people}
+        resourceName="person"
+        itemComponent={LargePersonListItem}
+      />
+          
+       </>
+    )
+}
+
+
+
+export default App;
+
+
+
+/*const LeftHandCompenent = ({ name }) => {
   return <h1 style={{ backgroundColor: '#DAF7A6' }}>{name}</h1>
 }
 
 const RightHandComponent = ({ message }) => {
   return <h1 style={{ backgroundColor: '#CCCCFF'}}>{message}</h1>
 }
-/* function App() {
+ function App() {
   return (
       <SplitScreen 
           left={LeftHandCompenent}
@@ -18,7 +92,7 @@ const RightHandComponent = ({ message }) => {
           rightWeight={3}
       />
   );
-} */
+} 
 
 function App() {
   return (
@@ -27,6 +101,4 @@ function App() {
           <RightHandComponent message="Hello"/>
       </SplitScreen>
   );
-} 
-
-export default App;
+}  */
