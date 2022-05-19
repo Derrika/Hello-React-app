7433510d1c7c9f37437fbdf839120cfa0aea61a1
Derrika <coley.derrika79@gmail.com>
2022-05-18 20:23:04 -0500

    update: added Numbereddlist

--- react-design-patterns/src/App.js
+++ react-design-patterns/src/App.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { RegularList } from './RegularList';
 import { SmallPersonListItem } from "./people/SmallPersonListItem";
 import { LargePersonListItem } from './people/LargePersonListItem';
+import { SmallProductListItem } from './products/SmallProductListItem';
+import { LargeProductListItem } from './products/LargeProductListItem';
+import { NumberedList } from './NumberedList';
 
 //import { SplitScreen } from './SplitScreen';
 
@@ -60,21 +63,20 @@ function App() {
           resourceName="person"
           itemComponent={SmallPersonListItem}
        />
+       <NumberedList 
+        items={products}
+        resourceName="product"
+        itemComponent={SmallProductListItem}
+      />
       <RegularList 
         items={people}
         resourceName="person"
         itemComponent={LargePersonListItem}
       />
-
-      <RegularList 
+      <NumberedList
         items={products}
         resourceName="product"
-        itemComponent={SmallPersonListItem}
-      />
-      <RegularList 
-        items={products}
-        resourceName="product"
-        itemComponent={LargePersonListItem}
+        itemComponent={LargeProductListItem}
       />
             
        </>
@@ -87,29 +89,4 @@ export default App;
 
 
 
-/*const LeftHandCompenent = ({ name }) => {
-  return <h1 style={{ backgroundColor: '#DAF7A6' }}>{name}</h1>
-}
 
-const RightHandComponent = ({ message }) => {
-  return <h1 style={{ backgroundColor: '#CCCCFF'}}>{message}</h1>
-}
- function App() {
-  return (
-      <SplitScreen 
-          left={LeftHandCompenent}
-          right={RightHandComponent}
-          leftWeight={1}
-          rightWeight={3}
-      />
-  );
-} 
-
-function App() {
-  return (
-      <SplitScreen leftWeight={1} rightWeight={3}>
-          <LeftHandCompenent name="Dasean"/>
-          <RightHandComponent message="Hello"/>
-      </SplitScreen>
-  );
-}  */
--- react-design-patterns/src/SplitScreen.js
+++ react-design-patterns/src/SplitScreen.js
@@ -45,3 +45,30 @@ export const SplitScreen = ({
         </Container>
     );
 }
+
+/*const LeftHandCompenent = ({ name }) => {
+  return <h1 style={{ backgroundColor: '#DAF7A6' }}>{name}</h1>
+}
+
+const RightHandComponent = ({ message }) => {
+  return <h1 style={{ backgroundColor: '#CCCCFF'}}>{message}</h1>
+}
+ function App() {
+  return (
+      <SplitScreen 
+          left={LeftHandCompenent}
+          right={RightHandComponent}
+          leftWeight={1}
+          rightWeight={3}
+      />
+  );
+} 
+
+function App() {
+  return (
+      <SplitScreen leftWeight={1} rightWeight={3}>
+          <LeftHandCompenent name="Dasean"/>
+          <RightHandComponent message="Hello"/>
+      </SplitScreen>
+  );
+}  */
