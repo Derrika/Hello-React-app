375a59fbd9c82e6673751a18e347806fd359f9ad
Derrika <coley.derrika79@gmail.com>
2022-05-21 09:38:00 -0500

    made change to app.js and DataSource.js filies

--- react-design-patterns/src/Container-Components/DataSource.js
+++ react-design-patterns/src/Container-Components/DataSource.js
@@ -1,5 +1,5 @@
 import React , { useState, useEffect } from "react";
-
+//import axios from 'axios';
 
 //Use when you're not sure where the data is coming  from
 export const DataSource = ({ getDataFunc = () => {}, resourceName, children }) => {
